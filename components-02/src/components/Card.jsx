95e810328532174304faa99c34b37074f97d960d
Hasnat2005 <hasnatashraf456@gmail.com>
2026-02-16 13:36:27 +0500

    components-02 folder done

---
+++ components-02/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className="Card">
+        <h1>Card Component</h1>
+        <p>This is a card component.</p>
+      </div>
+    </div>
+  )
+}
+
+export default card
---
+++ components-02/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+            <h3>This is my Navbar</h3>
+        </div>
+      
+    </div>
+  )
+}
+
+export { Navbar }
